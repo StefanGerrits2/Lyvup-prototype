ebc19222b802f893832c8067898ac3a537345b99
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-06-02 10:58:03 +0200

    fix styles

--- public/js/compliments.js
+++ public/js/compliments.js
@@ -1,23 +1,23 @@
 document.querySelector('.option1').addEventListener('click', () => {
     if (!document.querySelector('#feedback').checked) {
         document.querySelector('h2').textContent = 'Compliment geven';
-        document.querySelector('textarea').textContent = 'Typ hier je compliment';
+        document.querySelector('textarea').placeholder = 'Typ hier je compliment';
     }
 
     else {
         document.querySelector('h2').textContent = 'Feedback vragen';
-        document.querySelector('textarea').textContent = 'Vraag hier om feedback';
+        document.querySelector('textarea').placeholder = 'Vraag hier om feedback';
     }
 });
 
 document.querySelector('.option2').addEventListener('click', () => {
     if (!document.querySelector('#feedback').checked) {
         document.querySelector('h2').textContent = 'Compliment geven';
-        document.querySelector('textarea').textContent = 'Compliment geven';
+        document.querySelector('textarea').placeholder = 'Typ hier je compliment';
     }
 
     else {
         document.querySelector('h2').textContent = 'Feedback vragen';
-        document.querySelector('textarea').textContent = 'Vraag hier om feedback';
+        document.querySelector('textarea').placeholder = 'Vraag hier om feedback';
     }
 });
