f3329548a6d9f2b2b435a6756095c34881af0d9d
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-06-03 16:26:40 +0200

    add fallback

--- public/js/feedback.js
+++ public/js/feedback.js
@@ -1,3 +1,30 @@
-document.querySelector('.filter').addEventListener('click', () => {
-    document.querySelector('.filter__container').classList.toggle('show');
-});
+const filterContainer = document.querySelector('.filter__container');
+const filterButton = document.querySelector('.filter-button');
+
+filterContainer.classList.add('hide');
+filterButton.classList.add('show');
+
+if (document.addEventListener) {
+    // Eventlistener exists
+    filterButton.addEventListener('click', toggleFilter);
+    filterButton.addEventListener('keypress', toggleFilter);
+    
+}
+    
+else if (document.attachEvent) {              
+    // Eventlistener does not exist -> use attachEvent
+    filterButton.attachEvent('click', toggleFilter);
+    filterButton.attachEvent('keypress', toggleFilter);
+}
+
+function toggleFilter() {
+    if (filterContainer.classList.contains('hide')) {
+        filterContainer.classList.remove('hide');
+        filterContainer.classList.add('show');
+    }
+
+    else {
+        filterContainer.classList.add('hide');
+        filterContainer.classList.remove('show');
+    }
+}
--- routes/compliment.js
+++ routes/compliment.js
@@ -18,6 +18,18 @@ async function compliment(req, res) {
             data: userTeams
         });
 
+        // // Fetch users
+        // // Get all teams
+        // let user = await Fetcher.get(`https://lyvup.com/api/getUsersOfTeam?token=${process.env.TOKEN}&lang=dutch`, {
+        //     body: '{\n\
+        //         "getUsersOfTeam": {\n\
+        //           "search_member": "Stefan Gerrits"\n\
+        //         }\n\
+        //       }'
+        // });
+
+        // console.log('stefan log', user);
+
         console.log(req.body);
     } catch (error) {
         res.render('giveCompliment.hbs', {
