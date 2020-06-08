215eca72c051258c22b90bbef5964ed4d53e65f5
Michel <michel_abdad@hotmail.com>
2020-06-08 16:38:58 +0200

    work on card styling and collapsibles

---
+++ public/js/home.js
@@ -0,0 +1,15 @@
+const coll = document.getElementsByClassName('collapsible');
+let i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        const content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+            this.blur();
+        } else {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        }
+    });
+}
