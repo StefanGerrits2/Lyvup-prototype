14a3241d690a37de96b2fbc0ce0c2d5bfb024e02
Monika <monika_streljajeva@hotmail.com>
2020-06-08 18:40:47 +0200

    add feedback from friday 5.06.2020

--- public/js/team.js
+++ public/js/team.js
@@ -2,29 +2,9 @@ const profile__container = document.querySelectorAll('.profile__container');
 
 profile__container.forEach((button) => {
     button.addEventListener('click', () => {
-        let item_index = Array.prototype.indexOf.call(profile__container, button);
-
-        console.log(item_index)
-
-        // Add 1 to (index+1) until it reaches the clicked item
-        let plus = 1;
-        profile__container.forEach(function (button, index) {
-            console.log(button);
-            if (index < 1) {
-                plus = 0;
-            } else if (index == 1) {
-                button.style.order = plus += 2;
-                if (index = 2) {
-                    button.style.order = index - 1;
-                } else if (index = 3) {
-                    button.style.order = index - 2;
-                }
-            } else if (index > 2) {
-                button.style.order = index + 2;
-            }
-        });
+        let card_index = Array.prototype.indexOf.call(profile__container, button);
+        console.log(card_index);
 
         button.classList.toggle('collapse');
-
     });
 });
