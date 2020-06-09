85b16e403255a51042a90a4f48e5f09eaae9b0f4
Michel <michel_abdad@hotmail.com>
2020-06-09 15:33:01 +0200

    final dashboard

--- public/js/home.js
+++ public/js/home.js
@@ -2,10 +2,10 @@ const coll = document.getElementsByClassName('collapsible');
 let i;
 
 for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener('click', function() {
+    coll[i].addEventListener('click', function () {
         this.classList.toggle('active');
         const content = this.nextElementSibling;
-        if (content.style.maxHeight){
+        if (content.style.maxHeight) {
             content.style.maxHeight = null;
             this.blur();
         } else {
@@ -13,3 +13,24 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+
+
+
+const dots = document.getElementById('dots');
+const moreText = document.getElementById('more');
+const moreButton = document.getElementById('moreButton');
+
+moreButton.addEventListener('click', function () {
+    if (dots.style.display === 'none') {
+        dots.style.display = 'inline';
+        moreButton.innerHTML = 'Lees meer';
+        moreText.style.display = 'none';
+        moreButton.blur();
+    } else {
+        dots.style.display = 'none';
+        moreButton.innerHTML = 'Lees minder';
+        moreText.style.display = 'inline';
+        moreButton.blur();
+    }
+})
