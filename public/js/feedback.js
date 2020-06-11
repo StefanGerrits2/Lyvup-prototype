ab7cd825e876e975aaf967f6ab27540baf630fc1
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-06-11 12:39:17 +0200

    re design feedback page

--- public/js/feedback.js
+++ public/js/feedback.js
@@ -1,34 +1,15 @@
-const filterContainer = document.querySelector('.filter__container');
-const filterButton = document.querySelector('.filter-button');
+document.querySelectorAll('#button__container label').forEach(item => {
+    item.addEventListener('click', showFilters);
+});
 
-filterContainer.classList.add('hide');
-filterButton.classList.add('show');
-
-if (document.addEventListener) {
-    // Eventlistener exists
-    filterButton.addEventListener('click', toggleFilter);
-    filterButton.addEventListener('keypress', toggleFilter);
+function showFilters () {
+    setTimeout(() => {
+        if(!document.querySelector('#button__container #received').checked) {
+            document.querySelector('#filter3').classList.add('hide');
+        }
     
-}
-    
-else if (document.attachEvent) {              
-    // Eventlistener does not exist -> use attachEvent
-    filterButton.attachEvent('click', toggleFilter);
-    filterButton.attachEvent('keypress', toggleFilter);
-}
-
-function toggleFilter() {
-    // Expand
-    if (filterContainer.classList.contains('hide')) {
-        filterContainer.classList.remove('hide');
-        filterContainer.classList.add('show');
-        filterContainer.classList.toggle('expandFilter');
-    }
-
-    //
-    else {
-        filterContainer.classList.add('hide');
-        filterContainer.classList.remove('show');
-        filterContainer.classList.toggle('expandFilter');
-    }
+        else {
+            document.querySelector('#filter3').classList.remove('hide');
+        }
+    }, 0);
 }
