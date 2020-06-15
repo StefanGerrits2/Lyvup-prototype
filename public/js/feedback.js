76699e10d222d2c9b4efc575bcde81e564c7b22b
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-06-15 12:49:57 +0200

    implement feedback

--- public/js/feedback.js
+++ public/js/feedback.js
@@ -1,3 +1,5 @@
+const messages = document.querySelectorAll('.message');
+
 document.querySelectorAll('#button__container label').forEach(item => {
     item.addEventListener('click', showFilters);
 });
@@ -6,10 +8,18 @@ function showFilters () {
     setTimeout(() => {
         if(!document.querySelector('#button__container #received').checked) {
             document.querySelector('#filter3').classList.add('hide');
+
+            messages.forEach(item => {
+                item.setAttribute('style', 'display: none');
+            });
         }
     
         else {
             document.querySelector('#filter3').classList.remove('hide');
+
+            messages.forEach(item => {
+                item.setAttribute('style', 'display: flex');
+            });
         }
     }, 0);
 }
@@ -18,4 +28,31 @@ document.querySelectorAll('label').forEach(item => {
     item.addEventListener('keypress', () => {
         item.click();
     });
+});
+
+
+const toggleMembers = document.querySelector('#filter2 p');
+const hiddenLabels = document.querySelectorAll('.hiddenLabels');
+
+let toggleMembersState = true;
+
+toggleMembers.addEventListener('click', () => {
+
+    if(toggleMembersState) {
+        toggleMembers.textContent = 'Toon minder leden';
+        toggleMembersState = false;
+
+        hiddenLabels.forEach(item => {
+            item.setAttribute('style', 'display: flex');
+        });
+    }
+
+    else {
+        toggleMembers.textContent = 'Toon alle leden';
+        toggleMembersState = true;
+
+        hiddenLabels.forEach(item => {
+            item.setAttribute('style', 'display: none');
+        });
+    }
 });
